eac9a64a60121b10891ce3495374292ed8b62890
James Wilson <14128934+jmboby@users.noreply.github.com>
2026-05-27 10:36:42 +1200

    WIP

--- main.go
+++ main.go
@@ -8,8 +8,14 @@ func main() { // Defines the main function, which is the entry point of the Go p
 	mux := http.NewServeMux() // Creates a new HTTP request multiplexer (router) that matches incoming requests against registered handlers
 	// Handler (noun) = an object that implements the http.Handler interface (has a ServeHTTP method)
 	// Handle (verb) = the method used to register a handler for a specific URL pattern
-	mux.Handle("/", http.FileServer(http.Dir("."))) // Registers a file server handler on the root path "/" that serves files from the current directory
+	mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir(".")))) // Registers a file server handler on the root path "/" that serves files from the current directory
 	mux.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("assets"))))
+	
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, req *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8") // normal header
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	})
 
 	server := &http.Server{ // Creates a new HTTP server struct and configures its fields
 		Addr:    ":8080", // Sets the network address to listen on (port 8080 on all interfaces, e.g., localhost:8080)
